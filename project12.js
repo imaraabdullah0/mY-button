77f1ee952bce5ea2099a4b33c4dbe4319489ee51
imara abdullah <102338647+imaraabdullah0@users.noreply.github.com>
2022-11-04 17:22:36 +0200

    Add files via upload

---
+++ project12.js
@@ -0,0 +1,4 @@
+const containerE1 = document.querySelector(".container");
+const btnE1 = document.querySelector(".btn");
+const popupContainerE1 = document.querySelector(".popup-container");
+const closeIconE1 -document.querySelector(".close-icon");
